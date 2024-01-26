78cd22e8999061bee047e64603f42b909d8dabd3
Stheven <sthevenalvess@gmail.com>
2024-01-26 18:51:45 -0300

    feat: create notification

---
+++ src/main/java/com/picpaysimplified/dtos/NotificationDTO.java
@@ -0,0 +1,4 @@
+package com.picpaysimplified.dtos;
+
+public record NotificationDTO(String email, String message) {
+}
---
+++ src/main/java/com/picpaysimplified/servicies/NotificationService.java
@@ -0,0 +1,31 @@
+package com.picpaysimplified.servicies;
+
+import com.picpaysimplified.domain.user.User;
+import com.picpaysimplified.dtos.NotificationDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class NotificationService {
+    @Autowired
+    RestTemplate restTemplate;
+
+
+    public void sendNotification(User user, String message) throws Exception {
+        String email = user.getEmail();
+        NotificationDTO notificationRequest = new NotificationDTO(email, message);
+
+//        ResponseEntity<String> notificationResponse = restTemplate.postForEntity
+//                ("https://run.mocky.io/v3/54dc2cf1-3add-45b5-b5a9-6bf7e7f1f4a6",
+//                        notificationRequest, String.class);
+//
+//        if(!(notificationResponse.getStatusCode() == HttpStatus.OK)){
+//            System.out.println("Erro ao enviar notificacao");
+//            throw new Exception("Serviço de notificação estar fora do ar");
+//        }
+        System.out.println("notificacao enviada para o Usuario");
+    }
+}
