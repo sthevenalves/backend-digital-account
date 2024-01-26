0fac1a210772332f6018b16a1cab048a1f495484
Stheven <sthevenalvess@gmail.com>
2024-01-26 15:27:58 -0300

    inicial config

---
+++ src/test/java/com/picpaysimplified/PicpaySimplifiedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpaysimplified;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpaySimplifiedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
